ee6a822965e426ff2a34dac73c4d487489d3e97f
songtianyi <songtianyi630@163.com>
2018-09-19 11:01:01 +0800

    rename var

--- check.sh
+++ check.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-#!/bin/bash
-myheader="// Copyright 2018 golory Authors @1pb.club. All Rights Reserved.\n\
+set -o errexit -o nounset
+
+license="// Copyright 2018 golory Authors @1pb.club. All Rights Reserved.\n\
 //\n\
 // Licensed under the Apache License, Version 2.0 (the \"License\");\n\
 // you may not use this file except in compliance with the License.\n\
@@ -15,7 +16,7 @@ myheader="// Copyright 2018 golory Authors @1pb.club. All Rights Reserved.\n\
 // See the License for the specific language governing permissions and\n\
 // limitations under the License.\n"
 
-set -o errexit -o nounset
 # check format
-# check license header
-find . -name \*.go -exec sh -c "if ! grep -q 'LICENSE-2.0' '{}';then mv '{}' tmp && echo '$myheader' > '{}' && cat tmp >> '{}' && rm tmp;fi" \;
+
+# add license header
+find . -name \*.go -exec sh -c "if ! grep -q 'LICENSE-2.0' '{}';then mv '{}' tmp && echo '$license' > '{}' && cat tmp >> '{}' && rm tmp;fi" \;
