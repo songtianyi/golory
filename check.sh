f8e6607a576e434c61d9d0713039d114cf73067c
songtianyi <songtianyi630@163.com>
2018-09-26 13:21:26 +0800

    remove checkLicense func

--- check.sh
+++ check.sh
@@ -5,12 +5,5 @@ set -o errexit -o nounset
 # check format
 #TODO
 
-
 # check license header
-function checkLicense() {
-    file=$1
-	echo $file
-	if ! grep -q 'LICENSE-2.0' $file; then echo no license header in $file, run addlicense.sh to add; exit 1;fi
-}
-export -f checkLicense
-find . -name \*.go | xargs -n 1 -P 10 -I {} sh -c 'checkLicense "$@"' _ {}
+find . -name \*.go | xargs -n 1 -P 10 -I {} sh -c 'file="$@"; if ! grep -q 'LICENSE-2.0' $file; then echo no license header in $file, run addlicense.sh to add; exit 1;fi' _ {}
