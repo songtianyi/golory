fb446d7fb3fb6fab19032fb35302bfa650040ff6
songtianyi <songtianyi630@163.com>
2018-09-26 12:08:11 +0800

    fix check.sh not exit correct when license not set

--- check.sh
+++ check.sh
@@ -3,6 +3,13 @@
 set -o errexit -o nounset
 
 # check format
+#TODO
 
-# add license header
-find . -name \*.go -exec sh -c "if ! grep -q 'LICENSE-2.0' '{}';then echo no license header in '{}', run addlicense.sh to add;exit 1;fi" \;
+
+# check license header
+function checkLicense() {
+    file=$1
+	if ! grep -q 'LICENSE-2.0' $file; then echo no license header in $file, run addlicense.sh to add; exit 1;fi
+}
+export -f checkLicense
+find . -name \*.go | xargs sh -c "checkLicense"
